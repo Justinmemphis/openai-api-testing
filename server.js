2f79d68d6a9497ec9f3b3971871f765931d9f22f
Justinmemphis <jcarter82@gmail.com>
2023-03-28 13:19:36 -0500

    change node server and web page to module format

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const path = require('path'); // Require the path module
+const openai = require('openai');
 
 const app = express();
+const port = 3000;
 
-// Use body-parser middleware to parse request body
-app.use(bodyParser.json());
+// Set up OpenAI API credentials
+openai.apiKey = 'process.env.OPENAI_API_KEY';
 
-// Serve the static HTML file
+// Serve the HTML file when the root URL is requested
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 
-// POST endpoint for OpenAI API
-app.post('/openai', async (req, res) => {
-  try {
-    const { prompt } = req.body; // Get prompt from request body
+// Handle form submission
+app.get('/submit', (req, res) => {
+  const prompt = req.query.prompt;
 
-    // Call OpenAI API with prompt
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      prompt: prompt,
-      max_tokens: 10,
-      n: 1,
-      stop: '\n'
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}` // Replace with your OpenAI API key
-      }
-    });
+  // Call the OpenAI API with the prompt
+  openai.completion.create({
+    engine: 'text-davinci-002',
+    prompt: prompt,
+    maxTokens: 100,
+    n: 1,
+    stop: ['\n']
+  })
+  .then(response => {
+    const text = response.data.choices[0].text;
 
-    // Return API response
-    res.status(200).json({ response: response.data.choices[0].text });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
+    // Display the result to the user
+    res.send(text);
+  })
+  .catch(error => {
+    console.log(error);
+    res.send('An error occurred');
+  });
 });
 
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
